47a45d9425980770d221f288387995811ef6a30a
irailtonreis <=>
2019-11-27 13:04:35 -0300

    Alterações rota

--- src/Pages/Main/index.js
+++ src/Pages/Main/index.js
@@ -77,7 +77,7 @@ export default class Main extends Component {
           { repositories.map(repository => (
             <li key={repository.name}>
               <span>{repository.name}</span>
-              <Link to={`/repository/${encodeURIComponent(repository.name)}`}>Detalhes</Link>
+              <Link to={`/rockethub/repository/${encodeURIComponent(repository.name)}`}>Detalhes</Link>
             </li>
           ))
           }
