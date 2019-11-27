17a1cf61124ab1f824b0c5b00899426f7848f452
irailtonreis <=>
2019-11-27 13:08:53 -0300

    Alterações rota

--- src/Pages/Repository/index.js
+++ src/Pages/Repository/index.js
@@ -30,8 +30,8 @@ export default class Repository extends Component{
       const repoName = decodeURIComponent(match.params.repository);
 
     const [repository, issues] = await Promise.all([
-        api.get(`/hockethub/repos/${repoName}`),
-        api.get(`/hockethub/repos/${repoName}/issues`, {
+        api.get(`/repos/${repoName}`),
+        api.get(`/repos/${repoName}/issues`, {
           params: {
             state: 'open',
             per_page: 5,
