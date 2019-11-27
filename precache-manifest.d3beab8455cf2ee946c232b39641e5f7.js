85125d407100dbc964a0717220e81494c4f5feaa
irailtonreis <=>
2019-11-27 11:43:03 -0300

    Updates

---
+++ precache-manifest.d3beab8455cf2ee946c232b39641e5f7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "007737eea464539267046885ef7c5e6c",
+    "url": "/rockethub/index.html"
+  },
+  {
+    "revision": "82138349b7cac54688a0",
+    "url": "/rockethub/static/js/2.549f93a4.chunk.js"
+  },
+  {
+    "revision": "5c1d10620ac23d56cdb4",
+    "url": "/rockethub/static/js/main.464bd104.chunk.js"
+  },
+  {
+    "revision": "f2d774c7abf60cc1d024",
+    "url": "/rockethub/static/js/runtime-main.ee672465.js"
+  }
+]);
